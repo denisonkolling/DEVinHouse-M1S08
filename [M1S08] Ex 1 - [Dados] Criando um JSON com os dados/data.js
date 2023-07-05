234c57d27e25d7250296cb28c6662f5c44181d03
Denison Kolling <denisonkolling@gmail.com>
2023-07-05 07:48:21 -0300

    first commit

---
+++ [M1S08] Ex 1 - [Dados] Criando um JSON com os dados/data.js
@@ -0,0 +1,17 @@
+const users = [
+	{
+		name: 'Johnn',
+		email: 'john@email.com',
+		password: 1234,
+	},
+	{
+		name: 'Peter',
+		email: 'peter@email.com',
+		password: 1234,
+	},
+	{
+		name: 'Andrea',
+		email: 'andrea@email.com',
+		password: 1234,
+	},
+];
