25b8e5143d433d4f121cda7a572a027133393382
Denison Kolling <denisonkolling@gmail.com>
2023-07-10 09:12:39 -0300

    clean code comments

--- [M1S08] Ex 3 - [Lógica] Criando função validarLogin/login.js
+++ [M1S08] Ex 3 - [Lógica] Criando função validarLogin/login.js
@@ -31,27 +31,3 @@ loginSubmit.addEventListener('submit', function validateLogin(e) {
 		alert('Incorret email or password!');
 	}
 });
-
-// ### Funcao com eventListener no btn
-
-/* const loginBtn = document.querySelector('#btn-login');
-loginBtn.addEventListener('click', function login() {
-	let emailInput = document.querySelector('#email').value;
-	let passwordInput = document.querySelector('#password').value;
-	let validLogin = false;
-	for (let i in usersData) {
-		if (
-			emailInput == usersData[i].email &&
-			passwordInput == usersData[i].password
-		) {
-			validLogin = true;
-			let user_data = (usersData[i].name, usersData[i].email);
-			localStorage.setItem('user_data', user_data);
-			alert('Loged user!');
-			break;
-		} else {
-			alert('Incorret email or password!');
-			break;
-		}
-	}
-}); */
